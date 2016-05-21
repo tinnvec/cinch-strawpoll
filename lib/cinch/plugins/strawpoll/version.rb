f2e1e7a3923a8337093efc902f80e4a6b83c9303
Alex Hanna <tinnvec@gmail.com>
2016-05-21 10:02:50 -0500

    bump version to 0.1.6

--- lib/cinch/plugins/strawpoll/version.rb
+++ lib/cinch/plugins/strawpoll/version.rb
@@ -1,7 +1,10 @@
+# coding: utf-8
+# file: lib/cinch/plugins/strawpoll/version.rb
+
 module Cinch
-  module Plugins
-    module Strawpoll
-      VERSION = '0.0.9'
+    module Plugins
+        module Strawpoll
+            VERSION = '0.1.6'
+        end
     end
-  end
 end
