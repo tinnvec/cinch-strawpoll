5641acbb9535ca43417daa0a6369dc3728637d3e
Alex Hanna <tinnvec@gmail.com>
2016-05-21 10:39:26 -0500

    logic and syntax fixes

--- lib/cinch/plugins/strawpoll/api.rb
+++ lib/cinch/plugins/strawpoll/api.rb
@@ -4,32 +4,26 @@
 require 'httparty'
 require 'json'
 
-module Cinch
-    module Plugins
-        module Strawpoll
-            class Api
-                include HTTParty
+class StrawpollApi
+    include HTTParty
 
-                base_uri 'https://strawpoll.me/api/v2'
+    base_uri 'https://strawpoll.me/api/v2'
 
-                def create_poll(title, choices)
-                    response = create_poll_request(title, choices)
-                    return response
-                end
-
-                private
-
-                    def create_poll_request(title, choices)
-                        options = {
-                            headers: { 'Content-Type' => 'application/json' },
-                            body: {
-                                title: title, options: choices, multi: false
-                            }.to_json
-                        }
-                        response = self.class.post('/polls', options)
-                        return JSON.parse(response)
-                    end
-            end
-        end
+    def create_poll(title, choices)
+        response = create_poll_request(title, choices)
+        return response
     end
+
+    private
+
+        def create_poll_request(title, choices)
+            options = {
+                headers: { 'Content-Type' => 'application/json' },
+                body: {
+                    title: title, options: choices, multi: false
+                }.to_json
+            }
+            response = self.class.post('/polls', options)
+            return JSON.parse(response.body)
+        end
 end
