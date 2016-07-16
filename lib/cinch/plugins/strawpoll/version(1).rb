e493fc9d0c08536cafdd2c2da3b158d374622ce3
Alex Hanna <tinnvec@gmail.com>
2016-07-16 10:42:59 -0500

    intermediate commit

---
+++ lib/cinch/plugins/strawpoll/version(1).rb
@@ -0,0 +1,10 @@
+# coding: utf-8
+# file: lib/cinch/plugins/strawpoll/version.rb
+
+module Cinch
+    module Plugins
+        module Strawpoll
+            VERSION = '0.1.9'
+        end
+    end
+end
